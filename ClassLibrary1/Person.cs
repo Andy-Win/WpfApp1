24efd85a747c696fa9bbea95d7ab31db5f2046c4
v-wajian <v-wajian@andy27w11000>
2023-10-27 08:34:34 +0000

    Add project files.

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Person
+    { //Constructore
+        public Person()
+        {
+            this.Name = "Andy";
+            this.Age = 26;
+        }
+        //properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
